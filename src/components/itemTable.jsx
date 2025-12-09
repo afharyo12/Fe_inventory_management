958096e4ec36e99c6eca336107ff468c4e4c135f
afharyo <afharyodiwseno@gmail.com>
2025-12-09 22:59:41 +0700

    tambah api pada fe

--- src/components/itemTable.jsx
+++ src/components/itemTable.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { BASE_URL } from '../services/api';
 
-export default function ItemTable() {
-  const items = [
-    { id: 1, name: "Laptop", stock: 10, category: "Elektronik" },
-    { id: 2, name: "Mouse", stock: 25, category: "Aksesoris" },
-    { id: 3, name: "Meja Kantor", stock: 5, category: "Furniture" }
-  ];
+export default function ProductsTable() {
+  const [Products, setProducts] = useState([]);
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+  const getProducts = async () => {
+    //const token = localStorage.getProducts('token');
+    // if (!token) {
+    //   navigate('/login');
+    //   return;
+    // }
+
+    try {
+      const response = await fetch(`${BASE_URL}/products`, {
+        // headers: {
+        //   Authorization: `Bearer ${token}`,
+        // },
+      });
+
+      if (!response.ok) throw new Error('Failed to fetch Products');
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error('Error fetching Products:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  });
+
+  // const Productss = [
+  //   { id: 1, name: "Laptop", stock: 10, category: "Elektronik" },
+  //   { id: 2, name: "Mouse", stock: 25, category: "Aksesoris" },
+  //   { id: 3, name: "Meja Kantor", stock: 5, category: "Furniture" }
+  // ];
 
   return (
     <table style={{ width: "100%", borderCollapse: "collapse", marginTop: "20px" }}>
@@ -14,16 +44,14 @@ export default function ItemTable() {
           <th style={th}>ID</th>
           <th style={th}>Nama Barang</th>
           <th style={th}>Stok</th>
-          <th style={th}>Kategori</th>
         </tr>
       </thead>
       <tbody>
-        {items.map((item) => (
-          <tr key={item.id} style={{ background: "white" }}>
-            <td style={td}>{item.id}</td>
-            <td style={td}>{item.name}</td>
-            <td style={td}>{item.stock}</td>
-            <td style={td}>{item.category}</td>
+        {Products.map((Product) => (
+          <tr key={Product.id} style={{ background: "white" }}>
+            <td style={td}>{Product.id}</td>
+            <td style={td}>{Product.product_name}</td>
+            <td style={td}>{Product.qty}</td>
           </tr>
         ))}
       </tbody>
